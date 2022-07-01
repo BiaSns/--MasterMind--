2a49c8cc2901cd91b4f17953204b7705f3c54acf
BiaSns <106961151+BiaSns@users.noreply.github.com>
2022-07-01 12:00:16 +0200

    Add files via upload

--- MA$T3RMIND.c
+++ MA$T3RMIND.c
@@ -63,12 +63,13 @@ for(i = 0; i < CodeLenght; i++)
             if(SecretString[i] == DigitatedSequence[j]) //FACCIO IL CONFRONTO
             {
                 printf("Esito positivo tra %c e %c \n", SecretString[i], DigitatedSequence[j]);
-				ComparingVariable += 1;
+				ComparingVariable +=1;
 				FlagBianco +=1; // HO MESSO FLAGNERO E BIANCO SOLO PER INIZIARE A VISUALIZZARLI MA DEVO ANCORA CAPIRE COME E DOVE INCREMENTARLI E VICEVERSA.
 		        FlagNero +=1;
 				printf("\nIl valore di ComparingVariable = %d\n",ComparingVariable);//METTO LA STAMPA PER AVERE SOTT'OCCHIO DOVE LA VARIABILE ASSUME IL VALORE E PERCHE'
-				printf("\nFlagBianco = %d, FlagNero = %d",FlagBianco,FlagNero);
-				//break; break termina il loop -> occhio! 	
+				printf("\nFlagBianco = %d, FlagNero = %d\n",FlagBianco,FlagNero);
+				//break; break termina il loop -> occhio! 
+				 
 			}
             else {
                 printf("Esito negativo tra %c e %c\n ", SecretString[i], DigitatedSequence[j]);	
@@ -81,7 +82,7 @@ for(i = 0; i < CodeLenght; i++)
     
 
 	printf("\nIl valore finale assunto da ComparingVariable = %d\n",ComparingVariable); //QUESTA STAMPA E' PER CAPIRE CHE VALORE ASSUME COMPARING VARIABLE. A QUANTO PARE IL BUG E' NEL VALORE CHE DOVREBBE O MENO ASSUMERE QUESTA VARIABILE.
-	if(ComparingVariable == CodeLenght) //	CREDO CHE LA COSA MIGLIORE SIA CONTABILIZZARE IL CONTROLLO FINALE CON IL FLAG BIANCO COSI DA CORREGGERE 
+	if(FlagNero == CodeLenght) //	CREDO CHE LA COSA MIGLIORE SIA CONTABILIZZARE IL CONTROLLO FINALE CON IL FLAG BIANCO COSI DA CORREGGERE 
     {                                   //I BUG PRESENTI SULLA CONTA,PERMETTENDO COSIì DI GIOCARE CON QUALSIASI NUMERO.
         printf("\nHai indovinato il codice segreto :)");
     }
@@ -97,4 +98,6 @@ return 0;
 DEI PIOLI NERI E BIANCHI PER AVERE UNA LINEA GUIDA NELLO SVOLGIMENTO DEL GIOCO.
 CREDO CHE LA COSA MIGLIORE SIA CONTABILIZZARE IL CONTROLLO FINALE CON IL FLAG NERO(QUELLO CHE SEGNA POSIZIONE E LETTERA GIUSTA) COSI DA CORREGGERE
 I BUG PRESENTI SULLA CONTA,PERMETTENDO COSI' DI GIOCARE CON QUALSIASI NUMERO.
-HO MESSO FLAGNERO E BIANCO SOLO PER INIZIARE A VISUALIZZARLI MA DEVO ANCORA CAPIRE COME E DOVE INCREMENTARLI E VICEVERSA.*/
+HO MESSO FLAGNERO E BIANCO SOLO PER INIZIARE A VISUALIZZARLI MA DEVO ANCORA CAPIRE COME E DOVE INCREMENTARLI E VICEVERSA.
+CONVIENE USARE FLAGNERO COME VARIABILE COMPARATIVA FINALE PER STABILIRE VITTORIA O SCONFITTA*/
+
