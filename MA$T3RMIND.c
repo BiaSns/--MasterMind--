cbf5faffa656fec854ba3f7a38f0256ca410ecce
BiaSns <106961151+BiaSns@users.noreply.github.com>
2022-07-06 01:04:25 +0200

    Add files via upload

--- MA$T3RMIND.c
+++ MA$T3RMIND.c
@@ -22,7 +22,7 @@
 
 	char SecretStringColours [6] = {'R','Y','W','P','B','G'};  //Creo la struttura contenente i colori
 	int i;
-	int j;
+	//int j;
     int CodeLenght;
 	char SecretString[5]; //QUESTO ARRAY INVECE SALVERA' LA STRINGA RANDOM GENERATA
 	srand(time(NULL)); //Funzione che permette il funzionamento della funzione random 
@@ -38,66 +38,64 @@
     printf("\n%s",&SecretString);  //stampo la sequenza random
     
 	printf("\nINSERISCI LA TUA IPOTESI-RICORDA DI INSERIRE IN MAIUSCOLO\n\n");
-
-    char DigitatedSequence[sizeof CodeLenght]; 
-    scanf("%s", DigitatedSequence);
-   
-
-    int ComparingVariable=0;
+    
+	int ComparingVariable=0;
     int FlagBianco=0;
 	int FlagNero=0;
-
-for(i = 0; i < CodeLenght; i++)
-    {
-        printf("Giro della lettera n: %d\n",i);
-        printf("\nconfronto tra %c e %C \n", SecretString[i], DigitatedSequence[j]);
-         
-    {  
-
-       
-		for(j=0; j<CodeLenght; j++)
+	int N=1;
+   
+   do
+	{ 
+	printf("Tentativo N %d\n",N);
+	char DigitatedSequence[sizeof CodeLenght]; 
+    scanf("%s", DigitatedSequence);
+    N++;//VAriabile contatore per numero tentativi
+        for(i = 0; i < CodeLenght; i++)
         {
-            printf("\nconfronto tra %c e %C \n", SecretString[i], DigitatedSequence[j]);
-
-            
-            if(SecretString[i] == DigitatedSequence[j]) //FACCIO IL CONFRONTO
+        printf("\nconfronto tra %c e %c \n", SecretString[i], DigitatedSequence[i]);
+        //{  
+        //for(j=0; j<CodeLenght; j++)
+        // {
+         if(SecretString[i] == DigitatedSequence[i]) //FACCIO IL CONFRONTO
             {
-                printf("Esito positivo tra %c e %c \n", SecretString[i], DigitatedSequence[j]);
+            	printf("Esito positivo tra %c e %c \n", SecretString[i], DigitatedSequence[i]);
 				ComparingVariable +=1;
 				FlagBianco +=1; // HO MESSO FLAGNERO E BIANCO SOLO PER INIZIARE A VISUALIZZARLI MA DEVO ANCORA CAPIRE COME E DOVE INCREMENTARLI E VICEVERSA.
 		        FlagNero +=1;
 				printf("\nIl valore di ComparingVariable = %d\n",ComparingVariable);//METTO LA STAMPA PER AVERE SOTT'OCCHIO DOVE LA VARIABILE ASSUME IL VALORE E PERCHE'
 				printf("\nFlagBianco = %d, FlagNero = %d\n",FlagBianco,FlagNero);
-				//break; break termina il loop -> occhio! 
-				 
+				
+				if(ComparingVariable == CodeLenght) //E con questo if mi assicuro che se dovessi infovinare la sequenza si stopperanno i tentativi e il gioco finirà.
+				   {  
+				    printf("\nHai indovinato il codice segreto :)\n");
+					break; //Devo vedere come stoppare il loop ( DA sistemare)
+				   }
 			}
-            else {
-                printf("Esito negativo tra %c e %c\n ", SecretString[i], DigitatedSequence[j]);	
+        else    
+		        {
+                printf("Esito negativo tra %c e %c\n ", SecretString[i], DigitatedSequence[i]);	
                 printf("\nIl valore di ComparingVariable = %d\n",ComparingVariable);//METTO LA STAMPA PER AVERE SOTT'OCCHIO DOVE LA VARIABILE ASSUME IL VALORE E PERCHE'
-			    printf("\nFlagBianco = %d, FlagNero = %d",FlagBianco,FlagNero);
-				 }
-			}
-	    }
+			    printf("\nFlagBianco = %d, FlagNero = %d\n",FlagBianco,FlagNero);
+				}
+		
+		printf("\n Il codice digitato non corrisponde, riprova!\n");
+     	}
     }
     
-
-	printf("\nIl valore finale assunto da ComparingVariable = %d\n",ComparingVariable); //QUESTA STAMPA E' PER CAPIRE CHE VALORE ASSUME COMPARING VARIABLE. A QUANTO PARE IL BUG E' NEL VALORE CHE DOVREBBE O MENO ASSUMERE QUESTA VARIABILE.
-	if(FlagNero == CodeLenght) //	CREDO CHE LA COSA MIGLIORE SIA CONTABILIZZARE IL CONTROLLO FINALE CON IL FLAG BIANCO COSI DA CORREGGERE 
-    {                                   //I BUG PRESENTI SULLA CONTA,PERMETTENDO COSIì DI GIOCARE CON QUALSIASI NUMERO.
-        printf("\nHai indovinato il codice segreto :)");
-    }
-    else
-    {
-        printf("\nIl codice digitato non corrisponde, sei scarso.");
-    }
-
+	while(N<=9);
+	      {
+			 printf("\n Hai esaurito tutti i tentavi ed il codice digitato non corrisponde, sei scarso. )");  
+          }
 return 0;
 }
+  
 
-/*AGGIORNAMNETI: IL PROGRAMMA SEMBRA FUNZIONARE MA BISOGNA SISTEMARE ALCUNI BUG. RESTA DA CREARE DEI CONTATORI CHE GIOCANDO CON I CONTI STABILIRANNO L'ESISTENZA O MENO E LA QUANTITA'
-DEI PIOLI NERI E BIANCHI PER AVERE UNA LINEA GUIDA NELLO SVOLGIMENTO DEL GIOCO.
-CREDO CHE LA COSA MIGLIORE SIA CONTABILIZZARE IL CONTROLLO FINALE CON IL FLAG NERO(QUELLO CHE SEGNA POSIZIONE E LETTERA GIUSTA) COSI DA CORREGGERE
-I BUG PRESENTI SULLA CONTA,PERMETTENDO COSI' DI GIOCARE CON QUALSIASI NUMERO.
-HO MESSO FLAGNERO E BIANCO SOLO PER INIZIARE A VISUALIZZARLI MA DEVO ANCORA CAPIRE COME E DOVE INCREMENTARLI E VICEVERSA.
-CONVIENE USARE FLAGNERO COME VARIABILE COMPARATIVA FINALE PER STABILIRE VITTORIA O SCONFITTA*/
+	
 
+
+/*AGGIORNAMNETI: 
+HO DECISO DI SEPARARE LE LOGICHE(CONTROLLO STRINGA E ASSEGNAZIONI AI FLAG + CONTROLLO FINALE) PERCHE' SEMBRAVA CONFUSIONALE, HO FREEZATO IL SECONDO CILCO NELLA RIGA 57 FACENDO SCORRERE LE STRINGHE CON LO STESSO CONTATORE ORA CONFRONTO
+LETTERA PER LETTERA (CON LA RISPETTIVA SOTTO)-COSI' HO RISOLTO I BUG RELATIVI AL NORMALE FUNZIONAMENTO- ORA RESTA LA PARTE INTERESSANTE OVVERO LA PARTE FUNZIONALE DEI FLAG PER TENERE CONTO DEL RISULTATO
+AD OGNI TENTATIVO FATTO
+PRIMA DI PASSARE ALLA FUNZIONE DEI FLAG HO MESSO UN CICLO PER DARE UN MASSIMO DI NOVE TENTAIVI COSI' DA INIZIARE A SIMULARE IL VERO E PROPRIO GIOCO
+Devo vedere come stoppare il loop quando la sequenza è ordinata vedi riga 71 ( DA sistemare)*/
