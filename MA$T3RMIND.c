23f708e87a7a278593db8376498771f89edc36a9
BiaSns <106961151+BiaSns@users.noreply.github.com>
2022-07-07 11:36:43 +0200

    Add files via upload

--- MA$T3RMIND.c
+++ MA$T3RMIND.c
@@ -35,12 +35,12 @@
 		for(i=0; i<CodeLenght; i++){  
         SecretString[i]=SecretStringColours[rand()%(sizeof SecretStringColours)];
         }
-    printf("\n%s",&SecretString);  //stampo la sequenza random
+    printf("\n%s",&SecretString);  //stampo la sequenza random(MI SERVE IN FASE DI DEBUG)
     
 	printf("\nINSERISCI LA TUA IPOTESI-RICORDA DI INSERIRE IN MAIUSCOLO\n\n");
     
-	int ComparingVariable=0;
-    int FlagBianco=0;
+	//int ComparingVariable=0;
+    //int FlagBianco=0;
 	int FlagNero=0;
 	int N=1;
    
@@ -54,38 +54,43 @@
         {
         printf("\nconfronto tra %c e %c \n", SecretString[i], DigitatedSequence[i]);
         //{  
-        //for(j=0; j<CodeLenght; j++)
+        //for(j=0; j<CodeLenght; j++) QUESTO FOR LO USERò NELLA FUNZIONE TROVA FLAG BIANCHI
         // {
          if(SecretString[i] == DigitatedSequence[i]) //FACCIO IL CONFRONTO
             {
-            	printf("Esito positivo tra %c e %c \n", SecretString[i], DigitatedSequence[i]);
-				ComparingVariable +=1;
-				FlagBianco +=1; // HO MESSO FLAGNERO E BIANCO SOLO PER INIZIARE A VISUALIZZARLI MA DEVO ANCORA CAPIRE COME E DOVE INCREMENTARLI E VICEVERSA.
+            	printf("Esito positivo tra %c e %c \n", SecretString[i], DigitatedSequence[i]); 
 		        FlagNero +=1;
-				printf("\nIl valore di ComparingVariable = %d\n",ComparingVariable);//METTO LA STAMPA PER AVERE SOTT'OCCHIO DOVE LA VARIABILE ASSUME IL VALORE E PERCHE'
-				printf("\nFlagBianco = %d, FlagNero = %d\n",FlagBianco,FlagNero);
-				
-				if(ComparingVariable == CodeLenght) //E con questo if mi assicuro che se dovessi infovinare la sequenza si stopperanno i tentativi e il gioco finirà.
-				   {  
-				    printf("\nHai indovinato il codice segreto :)\n");
-					break; //Devo vedere come stoppare il loop ( DA sistemare)
-				   }
+				printf("\nIl valore di FlagNero = %d\n",FlagNero);//METTO LA STAMPA PER AVERE SOTT'OCCHIO DOVE LA VARIABILE ASSUME IL VALORE E PERCHE'
+				printf("\nFlagNero = %d\n",FlagNero);
 			}
-        else    
+        
+		else    
 		        {
                 printf("Esito negativo tra %c e %c\n ", SecretString[i], DigitatedSequence[i]);	
-                printf("\nIl valore di ComparingVariable = %d\n",ComparingVariable);//METTO LA STAMPA PER AVERE SOTT'OCCHIO DOVE LA VARIABILE ASSUME IL VALORE E PERCHE'
-			    printf("\nFlagBianco = %d, FlagNero = %d\n",FlagBianco,FlagNero);
+                printf("\nIl valore di FlagNero e' = %d\n",FlagNero);//METTO LA STAMPA PER AVERE SOTT'OCCHIO DOVE LA VARIABILE ASSUME IL VALORE E PERCHE'
+			    printf("FlagNero = %d\n",FlagNero);
+				printf("\nIl codice digitato non corrisponde, riprova!\n");
 				}
 		
-		printf("\n Il codice digitato non corrisponde, riprova!\n");
+		
      	}
-    }
+    if(FlagNero == CodeLenght){
+	                             printf("\nHai indovinato il codice segreto :)\n");
+								 break;  //QUESTA STRUTTURA SERVIRA' A TERMINARE IL LOOP NEL MOMENTO IN CUI INDOVINO IL CODICE SEGRETO
+	                          }
+      
+			                     
+        	                     
+
+	
+	FlagNero=0; //ASSEGNO ZERO ALLA VARIABILE ALLA FINE DI OGNI LOOP PER GARANTIRE IL CORRETTO FUNZIONAMENTO DELL'ALGORITMO
+	}
     
-	while(N<=9);
-	      {
-			 printf("\n Hai esaurito tutti i tentavi ed il codice digitato non corrisponde, sei scarso. )");  
-          }
+	while(N<=9); 
+	      
+  if (N=9 && FlagNero != CodeLenght){ //STRUTTURA DI CONTROLLO FINALE PER VEERIFICARE LA SCONFITTA
+	printf("\nHai esaurito tutti i tentavi ed il codice digitato non corrisponde, sei scarso.  :( "); 
+	}        
 return 0;
 }
   
@@ -98,4 +103,4 @@ HO DECISO DI SEPARARE LE LOGICHE(CONTROLLO STRINGA E ASSEGNAZIONI AI FLAG + CONT
 LETTERA PER LETTERA (CON LA RISPETTIVA SOTTO)-COSI' HO RISOLTO I BUG RELATIVI AL NORMALE FUNZIONAMENTO- ORA RESTA LA PARTE INTERESSANTE OVVERO LA PARTE FUNZIONALE DEI FLAG PER TENERE CONTO DEL RISULTATO
 AD OGNI TENTATIVO FATTO
 PRIMA DI PASSARE ALLA FUNZIONE DEI FLAG HO MESSO UN CICLO PER DARE UN MASSIMO DI NOVE TENTAIVI COSI' DA INIZIARE A SIMULARE IL VERO E PROPRIO GIOCO
-Devo vedere come stoppare il loop quando la sequenza è ordinata vedi riga 71 ( DA sistemare)*/
+RISOLTI TUTTI I BUG RELATIVI AL NORMALE FUNZIONAMENTO RESTA SOLO CREARE LA FUNZIONE CONTATORE DEI FLAGBIANCHI( QUELLI CHE SEGNANO LA PRESENZA DELLA LETTERA A PRESCINDERE DALL'ORDINE).
